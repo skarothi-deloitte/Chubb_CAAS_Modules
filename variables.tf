b332aa6c48bc7e3a21075c5204895d8fedbec7f3
skarothi-deloitte <77168247+skarothi-deloitte@users.noreply.github.com>
2021-04-09 09:32:26 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,65 +6,64 @@
 variable "Tanzu_Kubernetes_Cluster" {
   description = "Enter the 'Name' for Tanzu Kubernetes Cluster:"
   type        = string
-  # default = "job-tkc01"
+  default = ""
 }
 
 variable "Tanzu_Namespace" {
   description = "Select the Namespace value in Tanzu Grid you want to deploy this kubernetes cluster:"
   type        = string
-  # default = "nspace-terraform"
+  default = ""
 }
 
 
 variable "VM_Version" {
   description = "Select the VM Version:"
   type        = string
-  #default     = "v1.18.5+vmware.1-tkg.1.c40d30d"
+  default = ""
   
 }
 
 variable "Network_Name" {
   description = "Enter the name for the network connection:"
   type        = string
-  # default = "antrea"
-  # default = "calico"
+  default = ""
 }
 
 variable "Pod_CIDR" {
   description = "Define the CIDR range for the PODS"
   type        = string
-  # default     = "193.0.2.0/16"
+  default = ""
 }
 
 variable "ServiceDomain_CIDR" {
   description = "Define the CIDR range for the PODS"
   type        = string
-  # default     = "195.51.100.0/12"
+  default = ""
 }
 
 variable "VM_Class" {
   description = "Select the VM Class type:"
   type        = string
-  #default     = "best-effort-xsmall"
+  default = ""
 
 }
 
 variable "ControlPlane_Count" {
   description = "How many number of control planes you want?"
   type        = number
-  #default     = 1
+  default = ""
 }
 
 
 variable "Workers_Count" {
   description = "How many number of worker nodes you want?"
   type        = number
-  #default     = 2
+  default = ""
 }
 
 variable "Storage_Class" {
   description = "Enter the storage class value"
   type        = string
-  #default     = "pp-storage-policy"
+  default = ""
 }
 
