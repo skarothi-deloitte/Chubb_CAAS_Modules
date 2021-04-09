c956cf617de8855b53fe4f320f6d8ab95fb76f81
skarothi-deloitte <77168247+skarothi-deloitte@users.noreply.github.com>
2021-04-08 19:10:45 -0500

    Add files via upload

---
+++ tks.tf
@@ -0,0 +1,58 @@
+
+
+
+provider "kubernetes-alpha" {
+  config_path = "~/.kube/config" // path to kubeconfig
+
+}
+resource "kubernetes_manifest" "tanzukubernetescluster_job_tkc01" {
+  provider = kubernetes-alpha
+
+
+  /*"name"      = var.Tanzu_Kubernetes_Cluster
+      "namespace" = var.Tanzu_Namespace
+*/
+  manifest = {
+    "apiVersion" = "run.tanzu.vmware.com/v1alpha1"
+    "kind"       = "TanzuKubernetesCluster"
+    "metadata" = {
+      "name"      = ${var.Tanzu_Kubernetes_Cluster}
+      "namespace" = var.Tanzu_Namespace
+    }
+    "spec" = {
+      "distribution" = {
+        "version" = var.VM_Version
+      }
+      "settings" = {
+        "network" = {
+          "cni" = {
+            "name" = var.Network_Name
+          }
+          "pods" = {
+            "cidrBlocks" = [
+              var.Pod_CIDR,
+            ]
+          }
+          "serviceDomain" = "managedcluster.local"
+          "services" = {
+            "cidrBlocks" = [
+              var.ServiceDomain_CIDR,
+            ]
+          }
+        }
+      }
+      "topology" = {
+        "controlPlane" = {
+          "class"        = var.VM_Class
+          "count"        = var.ControlPlane_Count
+          "storageClass" = var.Storage_Class
+        }
+        "workers" = {
+          "class"        = var.VM_Class
+          "count"        = var.Workers_Count
+          "storageClass" = var.Storage_Class
+        }
+      }
+    }
+  }
+}
